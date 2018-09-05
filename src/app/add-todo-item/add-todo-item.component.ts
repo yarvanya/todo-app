7b225db988d4eb894e4205ada33089ff01d0ebaf
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2018-09-05 12:57:08 +0300

    Added search pipe

--- src/app/add-todo-item/add-todo-item.component.ts
+++ src/app/add-todo-item/add-todo-item.component.ts
@@ -1,13 +1,38 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {Todo} from '../todo';
+import {TodoDataService} from '../../services/todo-data.service';
 
 @Component({
   selector: 'app-add-todo-item',
   templateUrl: './add-todo-item.component.html',
-  styleUrls: ['./add-todo-item.component.css']
+  styleUrls: ['./add-todo-item.component.css'],
+  providers: [TodoDataService]
 })
-export class AddTodoItemComponent implements OnInit {
 
-  constructor() {}
+export class AddTodoItemComponent {
+  newTodo: Todo = new Todo();
+  searchText = '';
 
-  ngOnInit() {}
+  constructor(private todoDataService: TodoDataService) {}
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
+  get complete() {
+    return this.todoDataService.getCompleteTodos();
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule, MatCheckboxModule, MatListModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import {AddTodoItemComponent} from './add-todo-item/add-todo-item.component';
-
+import {SearchPipe} from './add-todo-item/search.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTodoItemComponent
+    AddTodoItemComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
